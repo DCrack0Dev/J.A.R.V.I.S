66f5f3d8fdc8dbed9e180942dc804a0b8094be95
DCrack0Dev <tebogopchakapedi@gmail.com>
2026-05-11 22:54:41 +0200

    feat(jobs): wire up JobsModule and register it in AppModule

--- backend/src/jobs/jobs.module.ts
+++ backend/src/jobs/jobs.module.ts
@@ -2,22 +2,10 @@ import { Module } from '@nestjs/common';
 import { JobsService } from './jobs.service';
 import { JobsController } from './jobs.controller';
 import { PrismaService } from '../prisma.service';
-import { ScraperService } from './scraper.service';
-import { LinkedInScraper } from './scrapers/linkedin.scraper';
-import { PNetScraper } from './scrapers/pnet.scraper';
-import { Careers24Scraper } from './scrapers/careers24.scraper';
-import { IndeedScraper } from './scrapers/indeed.scraper';
 
 @Module({
-  providers: [
-    JobsService, 
-    PrismaService, 
-    ScraperService,
-    LinkedInScraper,
-    PNetScraper,
-    Careers24Scraper,
-    IndeedScraper
-  ],
+  providers: [JobsService, PrismaService],
   controllers: [JobsController],
+  exports: [JobsService],
 })
 export class JobsModule {}
